2e92f1e2fb7ab96088ba1a974630880221b28f6e
nasy <naseefp.hin@gmail.com>
2022-08-31 20:50:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const user = {
+    first_name: "Naseef",
+    last_name: "leader",
+    email: "naseef@gmail.com",
+  };
+
+  const updteUser = {
+    first_name: "Naseef p",
+    last_name: "leader",
+    email: "naseef@gmail.com",
+  };
+
+  const addUser = async () => {
+    const resp = await axios.post(`https://reqres.in/api/users`, user);
+    console.log({ resp });
+  };
+
+  const getUsers = async () => {
+    const resp = await axios.get(
+      `https://reqres.in/api/users?page=1&per_page=10`
+    );
+    console.log({ resp });
+  };
+
+  const updateUser = async () => {
+    const resp = await axios.put(`https://reqres.in/api/users/55`, updteUser);
+    console.log({ resp });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <button onClick={addUser}>Post</button>
+        <button onClick={getUsers}>Get all</button>
+        <button onClick={updateUser}>update user</button>
+         <h2>To view the result check console!</h2>
     </div>
   );
 }
